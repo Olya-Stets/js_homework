894a609ef6caa8625c0171cd5869961e73c596c9
Olya Stets <90968930+Olya-Stets@users.noreply.github.com>
2025-03-30 15:28:08 -0400

    Add files via upload

---
+++ js_homework5.js
@@ -0,0 +1,43 @@
+var services = {
+    prices: {
+        "стрижка": "60 грн",
+        "гоління": "80 грн",
+        "Миття голови": "100 грн"
+    },
+
+    price: function () {
+        let total = 0;
+        for (let key in this.prices) { 
+            total += parseFloat(this.prices[key]);
+        }
+        return "Загальна сума = " + total + " грн";
+    },
+
+    minPrice: function () {
+        let min = Infinity;
+        for (let key in this.prices) {
+            let price = parseFloat(this.prices[key]);
+            if (price < min) {
+                min = price;
+            }
+        }
+        return "Мінімальна ціна = " + min + " грн";
+    },
+
+    maxPrice: function () {
+        let max = -Infinity;
+        for (let key in this.prices) {
+            let price = parseFloat(this.prices[key]);
+            if (price > max) {
+                max = price;
+            }
+        }
+        return "Максимальна ціна = " + max + " грн";
+    }
+};
+
+services.prices["New service"] = "300 грн";
+
+console.log(services.price());
+console.log(services.minPrice());
+console.log(services.maxPrice());
